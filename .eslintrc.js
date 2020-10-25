24c93fed1d60aebd6f2d4fa837d8d73f1d925907
Albert Gabdullin <gabdullin.aa@yandex.ru>
2020-10-25 18:12:20 +0300

    husky lint-staged config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,48 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true // Allows for the parsing of JSX
     }
   },
+  plugins: ['@typescript-eslint', 'import-quotes'],
   settings: {
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     }
   },
   extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'react-app'
   ],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'import-quotes/import-quotes': [1, 'single'],
+    'no-shadow': 'error',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'parent',
+          ['index', 'sibling']
+        ]
+      }
+    ]
   }
 };
