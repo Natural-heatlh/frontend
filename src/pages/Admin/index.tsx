b40b510797a8f627c11f928f1d51f65337f385c1
ainur <akhmetovainur@gmail.com>
2020-10-27 22:00:35 +0300

    add antd

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Button, DatePicker, version } from 'antd';
 
 const Admin = () => {
-  return <div>Hello world</div>;
+  return (
+    <div style={{ paddingTop: 30 }}>
+      <h1>antd version: {version}</h1>
+      <DatePicker/>
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </div>
+  )
 };
 
 export default Admin;
