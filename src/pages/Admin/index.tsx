cf7662c35145e9985398d818bad8eb0f27cdd557
ainur <akhmetovainur@gmail.com>
2020-10-27 22:20:23 +0300

    add styled components

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const Admin = () => {
-  return <div>Hello world</div>;
+  return <Title>Hello world</Title>;
 };
 
 export default Admin;
