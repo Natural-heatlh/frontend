8c0b3112a89d65ee76046958d05f960aad327809
Albert Gabdullin <gabdullin.aa@yandex.ru>
2021-01-16 19:24:51 +0300

    config node env

--- src/components/Admin/Add/Theory/SlideUploader.tsx
+++ src/components/Admin/Add/Theory/SlideUploader.tsx
@@ -1,8 +1,9 @@
+import React, { useCallback, useState } from 'react';
 import { Button, Form, message, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import React, { useCallback, useState } from 'react';
 import { UploadFile } from 'antd/es/upload/interface';
 import { UploadChangeParam } from 'antd/lib/upload';
+import { API_URL } from '../../../../helpers/getApiUrl';
 
 const SlideUploader = () => {
   const [slideList, updateSlideList] = useState<UploadFile[]>([]);
@@ -22,7 +23,7 @@ const SlideUploader = () => {
 
   const handleOnChange = useCallback(
     (info: UploadChangeParam) => {
-      console.log(info.fileList.filter(file => console.log(file)));
+      console.log(info.fileList.filter((file) => console.log(file)));
       updateSlideList(info.fileList.filter((file) => !!file.status));
     },
     [updateSlideList]
@@ -47,7 +48,7 @@ const SlideUploader = () => {
         fileList={slideList}
         onChange={handleOnChange}
         beforeUpload={beforeSlideUpload}
-        action="http://localhost:3000/upload-files"
+        action={`${API_URL}/upload-files`}
       >
         <Button icon={<UploadOutlined />}>Загрузить слайд</Button>
       </Upload>
